0b3dbf07920283a97c84c57470e943e8dfb949ff
Sophie. <sophie510380@gmail.com>
2025-12-01 21:21:10 -0500

    fix: reemplazar credenciales inseguras en la conexión a la BD

--- config/conexion.php
+++ config/conexion.php
@@ -1,7 +1,7 @@
 <?php
 class Conexion {
     public static function conectar() {
-        $db = new mysqli("localhost", "root", "AdminSofia1234", "almacenderopa");
+        $db = new mysqli("localhost", "root", "yA7*JmP$42fz!Q", "almacenderopa");
         $db->set_charset("utf8");
         return $db;
     }
